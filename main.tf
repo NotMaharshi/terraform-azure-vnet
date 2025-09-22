095038a4461cef3e51412e4b85222c72b5a5003c
NotMaharshi <maharshi.bhatnagar@clouddrove.com>
2025-09-23 01:32:43 +0530

    fix: workflow

--- main.tf
+++ main.tf
@@ -14,7 +14,6 @@ module "labels" {
   extra_tags      = var.extra_tags
 }
 
-
 ##-----------------------------------------------------------------------------
 ## Virtual Network – Creates a VNet with optional DNS, BGP, and DDoS settings
 ##-----------------------------------------------------------------------------
